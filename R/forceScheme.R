f4d6223576f106c30fd0cca659235c2b7975f456
Francisco M. Fatore <fmfatore@gmail.com>
2014-08-03 00:00:00 +0000

    version 0.1.2

--- R/forceScheme.R
+++ R/forceScheme.R
@@ -1,6 +1,6 @@
-#' Force Scheme
+#' Force Scheme Projection
 #'
-#' Create a 2D representation of the data based on a dissimilarity matrix. A few
+#' Creates a 2D representation of the data based on a dissimilarity matrix. A few
 #' modifications have been made in relation to the method described in the
 #' literature: shuffled indices are used to minimize the order dependency
 #' factor, only a fraction of delta is used for better stability and a tolerance
@@ -10,9 +10,10 @@
 #'   symmetric matrix containing the dissimilarities.
 #' @param initial A initial 2D configuration (optional). A random configuration
 #'   will be created when omitted.
-#' @param maxIt The maximum number of iterations that the method will run.
+#' @param max.it The maximum number of iterations that the method will run.
 #' @param tol The tolerance for the accumulated error between iterations. Set it
-#'   to 0 to guarantee it will run maxIt times.
+#'   to 0 to guarantee it will run max.it times.
+#' @param verbose A flag that indicates if the progress should be printed.
 #' @return The 2D representation of the data.
 #'
 #' @references Eduardo Tejada, Rosane Minghim, Luis Gustavo Nonato: On improved
@@ -20,17 +21,22 @@
 #'   data sets. Information Visualization 2(4): 218-231 (2003)
 #'
 #' @examples
-#' result = forceScheme(eurodist)
+#' # Eurodist example
+#' proj = forceScheme(eurodist)
+#' plot(proj, type = "n", xlab ="", ylab ="", asp=1, axes=FALSE, main="")
+#' text(proj, labels(eurodist), cex = 0.6)
+#'
+#' # Iris example
+#' proj = forceScheme(dist(iris[,1:4]))
+#' plot(proj, col=iris$Species)
 #' @seealso \code{\link[stats]{dist}} (stats) and \code{\link[proxy]{dist}}
 #'   (proxy) for d computation
 #'
+#' @useDynLib mp
 #' @export
-forceScheme = function(d, initial=NULL, maxIt=50, tol=0.1) {
-  # define EPSILON (minimum distance value for d2)
-  EPSILON = 1E-5
-
-  # define the fraction of delta
-  fraction = 8.0
+forceScheme = function(d, initial=NULL, max.it=50, tol=0.1, verbose=F) {
+  EPSILON = 1E-5 # minimum distance between points
+  fraction = 8.0 # fraction of delta
 
   # convert d to a matrix
   dmat = as.matrix(d)
@@ -43,67 +49,24 @@ forceScheme = function(d, initial=NULL, maxIt=50, tol=0.1) {
     initial = matrix(runif(n*2,0,1), ncol=2)
   }
 
+  # check initial configuration
+  if (nrow(initial) != n) {
+    stop("Provided initial configuration does not match the input matrix length");
+  }
+
   p = initial
 
-  # set the previous delta sum as infinity
-  prevDeltaSum = 1/0
+  # switch core implementation
+  pC = .C("force_scheme",
+          p = as.numeric(p),
+          dmat = as.numeric(dmat),
+          n = as.integer(n),
+          max_it = as.integer(max.it),
+          tol = as.double(tol),
+          EPSILON = as.double(EPSILON),
+          fraction = as.double(fraction))$p
 
-  # iterate maxIt times
-  for (i in 1:maxIt) {
-    print(paste("Iteration:", i))
-
-    # create a shuffle array to minimize the order dependency factor
-    s_j = sample(n)
-
-    # for each point
-    for (j in 1:n) {
-      # get the point index
-      p1_index = s_j[j]
-
-      # create another shuffle array
-      s_k = sample(n)
-
-      # reset delta sum
-      deltaSum = 0
-
-      # for each point
-      for (k in 1:n) {
-        # get the second point index
-        p2_index = s_k[k]
-
-        # skip itself
-        if (p1_index == p2_index) {
-          next;
-        }
-
-        # get the distance between the points in R2 (euclidean distance)
-        diff = p[p2_index,] - p[p1_index,]
-        d2 = sqrt(sum(diff^2))
-
-        # set a minimum distance for d2
-        d2 = max(d2, EPSILON)
-
-        # get the distance between the points in RN
-        dn = dmat[p1_index, p2_index]
-
-        # calculate delta
-        delta = (dn - d2) / fraction
-
-        # move the point_k in the direction of the point_j
-        p[p2_index,] = p[p2_index,] + delta * (diff / d2)
-
-        # update delta sum
-        deltaSum = deltaSum + abs(delta)
-      }
-    }
-
-    # check for the tolerance stop criterion
-    if (abs(prevDeltaSum - deltaSum) < tol) {
-      print(paste("The difference of delta sum is less than tol:", abs(prevDeltaSum - deltaSum)))
-      break
-    }
-    prevDeltaSum = deltaSum
-  }
+  p = cbind(pC[1:n], pC[(n+1):(2*n)])
 
   # return the 2D rojection
   return(p)
